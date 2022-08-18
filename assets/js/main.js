352863c42abf1d2127ff35410d885a574906e75b
rirefat <rirefat.official@gmail.com>
2022-08-18 23:51:51 +0600

    done testimonials section

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,26 @@ function activeWork (){
 linkWork.forEach(L=> L.addEventListener('click', activeWork));
 
 /*======================================== SWIPER TESTIMONIAL ========================================*/
+let swiperTestimonial = new Swiper(".testimonial__container",{
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
 
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+        },        
+    },
+});
 
 /*=================================== SCROLL SECTIONS ACTIVE LI=======================================*/
 
